3593438afb2be095267a0975bdb37a9fe263a9fe
Rafał Krajewski <43422381+rafalk1703@users.noreply.github.com>
2019-01-13 19:36:12 +0100

    Update widoki.sql

--- widoki.sql
+++ widoki.sql
@@ -12,15 +12,15 @@ CREATE VIEW upcoming_and_ongoing_conferences
 
 CREATE VIEW most_popular_conferences
 	AS SELECT TOP 5 c.ConferenceDetailsID, c.ConferenceName FROM ConferenceDetails c JOIN DaysOfConference dc
-										 ON c.ConferenceDetailsID = dc.ConferenceDetailsID JOIN Reservations r
-										 ON dc.DayOfConferenceId = r.DayOfConferenceID
+							ON c.ConferenceDetailsID = dc.ConferenceDetailsID JOIN Reservations r
+							ON dc.DayOfConferenceId = r.DayOfConferenceID
 	   GROUP BY ConferenceDetailsID
 		 ORDER BY count(r.ReservationID) DESC
 
 
 CREATE VIEW most_popular_workshops
 	AS SELECT TOP 5 w.WorkShopID, w.WorkshopName FROM Workshops w JOIN Reservations r
-										 ON w.WorkshopID = r.WorkshopID
+							ON w.WorkshopID = r.WorkshopID
      GROUP BY WorkshopID
      ORDER BY count(r.ReservationID) DESC
 
@@ -39,6 +39,6 @@ CREATE VIEW show_only_students
 
 CREATE VIEW show_conference_days_with_free_seats
 	AS SELECT DayOfConferenceID, dc.QuantityOfSeats - sum(r.QuantityOfSeats) AS wolne_miejsca FROM DaysOfConferences dc JOIN Reservations r
-										 ON dc.DayOfConferencesID = r.DayOfConferencesID
+							ON dc.DayOfConferencesID = r.DayOfConferencesID
       GROUP BY dc.DayOfConferenceID
       HAVING sum(r.QuantityOfSeats) < dc.QuantityOfSeats
