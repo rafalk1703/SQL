b5856d94a6124a8c54712193121fd6dd28fa4ce5
Rafał Krajewski <43422381+rafalk1703@users.noreply.github.com>
2019-01-14 09:48:54 +0100

    Update triggery.sql

--- triggery.sql
+++ triggery.sql
@@ -1,25 +1,38 @@
-create trigger reservation_max_count
-  on Reservations
-  after insert,update
-  as
-  if exists(select *
+CREATE TRIGGER reservationMaxCount
+  ON Reservations
+  AFTER INSERT, UPDATE
+  AS
+  IF exists(select *
             from inserted i
             where dbo.conference_day_occupied_seats (i.DayOfConferenceID) >
             (select QuantityOfSeats from DaysOfConference d where d.DayOfConferenceID = i.DayOfConferenceID))
-      begin
+      BEGIN
         raiserror('Za mało wolnych miejsc', -1, 1)
         rollback transaction
-      end
+      END
 
-create trigger workshop_reservation_max_count
-  on Reservations
-  after insert,update
-  as
-  if exists(select *
+CREATE TRIGGER workshopReservationMaxCount
+  ON Reservations
+  AFTER INSERT, UPDATE
+  AS
+  IF exists(select *
             from inserted i
             where dbo.workshop_occupied_seats (i.WorkshopID) >
             (select w.QuantityOfSeats from Workshops w where w.Workshops.WorkshopID = i.WorkshopID))
-      begin
+      BEGIN
         raiserror('Za mało wolnych miejsc', -1, 1)
+        rollback transaction
+      END
+
+CREATE TRIGGER worshopBadDay
+  ON Workshops
+  AFTER INSERT
+  AS
+  IF exist(select *
+           from inserted i join DaysOfConference dc
+           on i.DayOfconferenceID = dc.DayOfConferenceID
+           where dc.Date < i.StartDate or dc.Date < i.EndDate)
+      BEGIN
+        raiserror ('Zła data warsztatów', -1, 1)
         rollback transaction
       end
